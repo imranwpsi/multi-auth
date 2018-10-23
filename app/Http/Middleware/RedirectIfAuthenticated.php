af774ce35e30a7479f554957ce86436be2150e53
Md. Imran Hossain <imran@idbb.org>
2018-10-23 09:38:51 +0600

    first commit

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+    public function handle($request, Closure $next, $guard = null)
+    {
+        
+        if ($guard == "admin" && Auth::guard($guard)->check()) {
+             // \Log::info('this is admin');
+            return redirect('/admin');
+        }
+        if ($guard == "writer" && Auth::guard($guard)->check()) {
+            return redirect('/writer');
+        }
+        if (Auth::guard($guard)->check()) {
+            return redirect('/home');
+        }
+
+        return $next($request);
+    }
+}
